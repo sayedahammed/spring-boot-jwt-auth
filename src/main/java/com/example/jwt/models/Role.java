4e4c89f65d69107cb4286bbf91d1a23733070766
Md. Sayed Ahammed <sayed.ahammed@reddotdigitalit.com>
2021-03-09 18:46:40 +0600

    initial commit

---
+++ src/main/java/com/example/jwt/models/Role.java
@@ -0,0 +1,22 @@
+package com.example.jwt.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole name;
+}
